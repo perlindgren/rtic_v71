78d47609e0c0f9b851d9c0f3921290252cde68b0
Per Lindgren <per.lindgren@ltu.se>
2022-05-27 14:52:36 +0200

    wip wip

--- examples/usb_serial.rs
+++ examples/usb_serial.rs
@@ -27,45 +27,45 @@ mod app {
         let dp = cx.device;
 
         let usb_bus = Usbd::new(dp.USBHS);
-        // let mut serial = SerialPort::new(&usb_bus);
+        let mut serial = SerialPort::new(&usb_bus);
 
-        // let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        //     .manufacturer("Fake company")
-        //     .product("Serial port")
-        //     .serial_number("TEST")
-        //     .device_class(USB_CLASS_CDC)
-        //     .max_packet_size_0(64) // (makes control transfers 8x faster)
-        //     .build();
+        let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")
+            .device_class(USB_CLASS_CDC)
+            .max_packet_size_0(64) // (makes control transfers 8x faster)
+            .build();
 
-        // loop {
-        //     if !usb_dev.poll(&mut [&mut serial]) {
-        //         continue;
-        //     }
+        loop {
+            if !usb_dev.poll(&mut [&mut serial]) {
+                continue;
+            }
 
-        //     let mut buf = [0u8; 64];
+            let mut buf = [0u8; 64];
 
-        //     match serial.read(&mut buf) {
-        //         Ok(count) if count > 0 => {
-        //             // Echo back in upper case
-        //             for c in buf[0..count].iter_mut() {
-        //                 if 0x61 <= *c && *c <= 0x7a {
-        //                     *c &= !0x20;
-        //                 }
-        //             }
+            match serial.read(&mut buf) {
+                Ok(count) if count > 0 => {
+                    // Echo back in upper case
+                    for c in buf[0..count].iter_mut() {
+                        if 0x61 <= *c && *c <= 0x7a {
+                            *c &= !0x20;
+                        }
+                    }
 
-        //             let mut write_offset = 0;
-        //             while write_offset < count {
-        //                 match serial.write(&buf[write_offset..count]) {
-        //                     Ok(len) if len > 0 => {
-        //                         write_offset += len;
-        //                     }
-        //                     _ => {}
-        //                 }
-        //             }
-        //         }
-        //         _ => {}
-        //     }
-        // }
+                    let mut write_offset = 0;
+                    while write_offset < count {
+                        match serial.write(&buf[write_offset..count]) {
+                            Ok(len) if len > 0 => {
+                                write_offset += len;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
 
         (Shared {}, Local {}, init::Monotonics())
     }
